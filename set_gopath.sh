c721c1ccdada74df484906fcad02df2432c0d04e
aakatev <aakatev@mail.sfsu.edu>
2019-02-24 19:23:10 +0000

    GOPATH qucik config

---
+++ set_gopath.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "export GORUN=/usr/local/go/bin" >> ~/.bash_profile
+echo "export GOPATH=$HOME/go" >> ~/.bash_profile
+echo "export GOBIN=$GOPATH/bin" >> ~/.bash_profile
+echo "export PATH=$PATH:$GOBIN" >> ~/.bash_profile
